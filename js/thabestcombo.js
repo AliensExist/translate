7139a7f7b58e6fab81f05b1c54e935f75f5874f7
Maggie <margaritakarlsson@yahoo.se>
2016-05-17 14:47:45 +0200

    created a new file for searchbar and buttonclick functions. Combined them and uptimized

---
+++ js/thabestcombo.js
@@ -0,0 +1,140 @@
+var ljud = [ /* Månaderna */
+            ["månaderna", "armånaderna", "<audio controls><source src='audio/månaderna/01månaderna.mp3' type='audio/mpeg'></audio>"],
+            ["januari", "armånaderna", "<audio controls><source src='audio/månaderna/02januari.mp3' type='audio/mpeg'></audio>"],
+            ["februari", "arfebruari", "<audio controls><source src='audio/månaderna/03februari.mp3' type='audio/mpeg'></audio>"],
+            ["mars", "armars", "<audio controls><source src='audio/månaderna/04mars.mp3' type='audio/mpeg'></audio>"],
+            ["april", "arapril", "<audio controls><source src='audio/månaderna/05april.mp3' type='audio/mpeg'></audio>"],
+            ["maj", "armaj", "<audio controls><source src='audio/månaderna/06maj.mp3' type='audio/mpeg'></audio>"],
+            ["juni", "arjuni", "<audio controls><source src='audio/månaderna/07juni.mp3' type='audio/mpeg'></audio>"],
+            ["juli", "arjuli", "<audio controls><source src='audio/månaderna/08juli.mp3' type='audio/mpeg'></audio>"],
+            ["augusti", "araugusti", "<audio controls><source src='audio/månaderna/09augusti.mp3' type='audio/mpeg'></audio>"],
+            ["september", "arseptember", "<audio controls><source src='audio/månaderna/10september.mp3' type='audio/mpeg'></audio>"],
+            ["oktober", "aroktober", "<audio controls><source src='audio/månaderna/11oktober.mp3' type='audio/mpeg'></audio>"],
+            ["november", "arnovember", "<audio controls><source src='audio/månaderna/12november.mp3' type='audio/mpeg'></audio>"],
+            ["december", "ardecember", "<audio controls><source src='audio/månaderna/13december.mp3' type='audio/mpeg'></audio>"],
+            /* Siffror */
+            ["0", "armånaderna", "<audio controls><source src='audio/Siffror/0.mp3' type='audio/mpeg'></audio>"],
+            ["1", "arfebruari", "<audio controls><source src='audio/Siffror/1.mp3' type='audio/mpeg'></audio>"],
+            ["2", "armars", "<audio controls><source src='audio/Siffror/2.mp3' type='audio/mpeg'></audio>"],
+            ["3", "arapril", "<audio controls><source src='audio/Siffror/3.mp3' type='audio/mpeg'></audio>"],
+            ["4", "armaj", "<audio controls><source src='audio/Siffror/4.mp3' type='audio/mpeg'></audio>"],
+            ["5", "arjuni", "<audio controls><source src='audio/Siffror/5.mp3' type='audio/mpeg'></audio>"],
+            ["6", "arjuli", "<audio controls><source src='audio/Siffror/6.mp3' type='audio/mpeg'></audio>"],
+            ["7", "araugusti", "<audio controls><source src='audio/Siffror/7.mp3' type='audio/mpeg'></audio>"],
+            ["8", "arseptember", "<audio controls><source src='audio/Siffror/8.mp3' type='audio/mpeg'></audio>"],
+            ["9", "aroktober", "<audio controls><source src='audio/Siffror/9.mp3' type='audio/mpeg'></audio>"],
+            ["10", "arnovember", "<audio controls><source src='audio/Siffror/10.mp3' type='audio/mpeg'></audio>"],
+            ["11", "ardecember", "<audio controls><source src='audio/Siffror/11.mp3' type='audio/mpeg'></audio>"],
+            ["12", "arseptember", "<audio controls><source src='audio/Siffror/12.mp3' type='audio/mpeg'></audio>"],
+            ["13", "aroktober", "<audio controls><source src='audio/Siffror/13.mp3' type='audio/mpeg'></audio>"],
+            ["14", "arnovember", "<audio controls><source src='audio/Siffror/14.mp3' type='audio/mpeg'></audio>"],
+            ["15", "ardecember", "<audio controls><source src='audio/Siffror/15.mp3' type='audio/mpeg'></audio>"],
+            ["16", "arseptember", "<audio controls><source src='audio/Siffror/16.mp3' type='audio/mpeg'></audio>"],
+            ["17", "aroktober", "<audio controls><source src='audio/Siffror/17.mp3' type='audio/mpeg'></audio>"],
+            ["18", "arnovember", "<audio controls><source src='audio/Siffror/18.mp3' type='audio/mpeg'></audio>"],
+            ["19", "ardecember", "<audio controls><source src='audio/Siffror/19.mp3' type='audio/mpeg'></audio>"],
+            ["20", "ardecember", "<audio controls><source src='audio/Siffror/20.mp3' type='audio/mpeg'></audio>"],
+            ["30", "arseptember", "<audio controls><source src='audio/Siffror/30.mp3' type='audio/mpeg'></audio>"],
+            ["40", "aroktober", "<audio controls><source src='audio/Siffror/40.mp3' type='audio/mpeg'></audio>"],
+            ["50", "arnovember", "<audio controls><source src='audio/Siffror/50.mp3' type='audio/mpeg'></audio>"],
+            ["60", "ardecember", "<audio controls><source src='audio/Siffror/60.mp3' type='audio/mpeg'></audio>"],
+            ["70", "ardecember", "<audio controls><source src='audio/Siffror/70.mp3' type='audio/mpeg'></audio>"],
+            ["80", "arseptember", "<audio controls><source src='audio/Siffror/80.mp3' type='audio/mpeg'></audio>"],
+            ["90", "aroktober", "<audio controls><source src='audio/Siffror/90.mp3' type='audio/mpeg'></audio>"],
+            ["100", "aroktober", "<audio controls><source src='audio/Siffror/100.mp3' type='audio/mpeg'></audio>"],
+            ["rakneforklarning", "arnovember", "<audio controls><source src='audio/Siffror/Rakneforklarning.mp3' type='audio/mpeg'></audio>"],
+            ["siffror", "ardecember", "<audio controls><source src='audio/Siffror/Siffror.mp3' type='audio/mpeg'></audio>"],
+            /* Veckodagar */
+            ["veckodagarna", "ardecember", "<audio controls><source src='audio/Veckodagarna/Veckodagarna.mp3' type='audio/mpeg'></audio>"],
+            ["måndag", "ardecember", "<audio controls><source src='audio/Veckodagarna/Måndag.mp3' type='audio/mpeg'></audio>"],
+            ["tisdag", "arseptember", "<audio controls><source src='audio/Veckodagarna/Tisdag.mp3' type='audio/mpeg'></audio>"],
+            ["onsdag", "aroktober", "<audio controls><source src='audio/Veckodagarna/Onsdag.mp3' type='audio/mpeg'></audio>"],
+            ["torsdag", "aroktober", "<audio controls><source src='audio/Veckodagarna/Torsdag.mp3' type='audio/mpeg'></audio>"],
+            ["fredag", "ardecember", "<audio controls><source src='audio/Veckodagarna/Fredag.mp3' type='audio/mpeg'></audio>"],
+            ["lördag", "arseptember", "<audio controls><source src='audio/Veckodagarna/Lördag.mp3' type='audio/mpeg'></audio>"],
+            ["Söndag", "aroktober", "<audio controls><source src='audio/Veckodagarna/Söndag.mp3' type='audio/mpeg'></audio>"]
+            /*  */
+          ];
+
+/************************** Search funktionen ***************************/
+
+function search() {
+  var vad = document.getElementById('here').value;
+  var lowVad = vad.toLowerCase();
+
+//var funkaDet = false;
+  for (var i = 0; i < ljud.length; i++) {
+      var a = ljud[i].indexOf(lowVad);
+
+      /* Om den hittar ska den säga var den hitta. */
+      if (a > -1) {
+       //funkarDet = true;
+       /* Gör till object som ska användas */
+       function File(svname,arname,ljud) {
+         this.svname = svname;
+         this.arname = arname;
+         this.ljud = ljud;
+       }
+       var arraY = new File(ljud[i][0],ljud[i][1],ljud[i][2]);
+
+       /* Skriva ut infon på sidan */
+       var printAll = function (p) {
+         var stronk = document.getElementById("platsen");
+            if (a === 0) {
+              stronk.innerHTML = stronk.innerHTML +
+              "Svenska ordet: <strong>" + p.svname.cap1Letter()  +
+              "</strong> Arabiska ordet: <strong>" +  p.arname.cap1Letter()  + "</strong><br>" + p.ljud + "<br>";
+            } else {
+             stronk.innerHTML = stronk.innerHTML +
+             "Arabiska ordet: <strong>" + p.arname.cap1Letter() +
+             "</strong> Svenska ordet: <strong>" + p.svname.cap1Letter()  + "</strong><br>" + p.ljud + "<br>";
+           }
+       };
+       printAll(arraY);
+       /* Hittar den inte skriv: */
+     } else {
+       //if(funkarDet === false) console.log("Hallå");
+       //else
+       console.log("Damn this");
+      }
+  }
+}
+
+/* Ta bort text ur input */
+function textClear(element)
+{ if ( element.value != '' ){
+      element.value = '';
+  }
+}
+
+/* Convertera svarsorden till första bokstaven uppercase */
+String.prototype.cap1Letter = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
+
+/* Enter klickar på knappen*/
+function runScript(e) {
+    if (e.keyCode == 13) {
+          document.getElementById('Den').click();
+    }
+}
+
+/****************************** Knapptryckningar (rad först, placeringen i sidled)*********************************/
+$("#mon1").click(function(){
+    $(".audioshow").show();
+    for (var t = 0; t < ljud.length; t++) {
+      console.log("Hära: " + ljud[1][t]);
+    }
+    document.getElementById("audioshow").innerHTML = "<p>" + ljud[2][0] + "</p>" + ljud[2][2] +
+                                                     "<br><p>" + ljud[3][0] + "</p>" + ljud[3][2] +
+                                                     "<br><p>" + ljud[4][0] + "</p>" + ljud[4][2] +
+                                                     "<br><p>" + ljud[5][0] + "</p>" + ljud[5][2] ;
+    });
+
+$("#mon2").click(function(){
+    $(".audioshow").show();
+    document.getElementById("audioshow").innerHTML = "<p>" + ljud[6][0] + "</p>" + ljud[6][2] +
+                                                     "<br><p>" + ljud[7][0] + "</p>" + ljud[7][2] +
+                                                     "<br><p>" + ljud[8][0] + "</p>" + ljud[8][2] +
+                                                     "<br><p>" + ljud[9][0] + "</p>" + ljud[9][2] ;
+    });
